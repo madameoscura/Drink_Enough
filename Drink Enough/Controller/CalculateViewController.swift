68bb0b5df0c9bdab8082f9574a60b9fedc97d0c8
Annekatrin Dunkel <annekatrin.dunkel@gmx.de>
2020-03-15 13:31:31 +0100

    Finalizing layout

--- Drink Enough/Controller/CalculateViewController.swift
+++ Drink Enough/Controller/CalculateViewController.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class CalculateViewController: UIViewController {
 
+    public string userWeightInKg { get; set; }
+    public static int mlToDrinkPerKg = 30;
+    public int finalAmountToDrink = new int();
+    public string calculatedAmount { get; set; }
+    JsonHelper jsonHelper = new JsonHelper();
+    Dictionary<string, int> jsonDict = new Dictionary<string, int>();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        calculatedAmount = Convert.ToString(int.Parse(userWeightInKg) * mlToDrinkPerKg);
+        WaterTxtInput.Text = calculatedAmount + " ml";
+
+        //Add observer to include "ml" behind numbers inserted
+        NSNotificationCenter.DefaultCenter.AddObserver(
+        UITextField.TextDidBeginEditingNotification, (notification) => {
+         var indexToSet = WaterTxtInput.Text.Length - 3;
+         var positionToSet = WaterTxtInput.GetPosition(WaterTxtInput.BeginningOfDocument, indexToSet);
+          WaterTxtInput.SelectedTextRange = WaterTxtInput.GetTextRange(positionToSet, positionToSet);
+        });
+
+        NSNotificationCenter.DefaultCenter.AddObserver(
+        UITextField.TextFieldTextDidChangeNotification, (notification) =>
+        {
+           if (!string.IsNullOrEmpty(WaterTxtInput.Text) && WaterTxtInput.Text.Length > 1)
+           {
+               if (WaterTxtInput.Text.Substring(WaterTxtInput.Text.Length - 2) == "ml")
+               {
+                   WaterTxtInput.Text = WaterTxtInput.Text;
+               }
+           }
+           else
+           {
+               WaterTxtInput.Text = WaterTxtInput.Text + " ml";
+           }
+           var indexToSet = WaterTxtInput.Text.Length - 3;
+           var positionToSet = WaterTxtInput.GetPosition(WaterTxtInput.BeginningOfDocument, indexToSet);
+           WaterTxtInput.SelectedTextRange = WaterTxtInput.GetTextRange(positionToSet, positionToSet);
+
+        });
+        
+        DoneButton.TouchUpInside += DoneButton_TouchUpInside;
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //What should happen when done Button is pressed
+    private void DoneButton_TouchUpInside(object sender, EventArgs e)
+    {
+        finalAmountToDrink = int.Parse(WaterTxtInput.Text.Remove(WaterTxtInput.Text.Length - 3, 3));
+        jsonDict["amount"] = finalAmountToDrink;
+        jsonDict["weight"] = int.Parse(userWeightInKg);
+        jsonHelper.jsonWrite(jsonDict);
+        Console.WriteLine(finalAmountToDrink);
+        Console.WriteLine(finalAmountToDrink);
     }
-    */
 
 }
--- Drink Enough/Controller/WeightViewController.swift
+++ Drink Enough/Controller/WeightViewController.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class WeightViewController: UIViewController {
 
+    DBHelper dbHelper = new DBHelper();
+     JsonHelper jsonHelper = new JsonHelper();
+    // Dictionary<string, int> jsonDict;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        dbHelper.createDB();
+
+        /*  Drink olddrink = new Drink()
+         {
+             AmountDrank = 1400,
+             DrinkingGoal = 3000,
+             CreateDate = new DateTime(2020,3,5)
+         };
+         dbHelper.insertDrink(olddrink); */
+
+         //Add observer to include "kg" behind numbers inserted
+         NSNotificationCenter.DefaultCenter.AddObserver(
+         UITextField.TextFieldTextDidChangeNotification, (notification) =>
+         { if (!string.IsNullOrEmpty(WeightTxtInput.Text) && WeightTxtInput.Text.Length > 1)
+             {
+              if (WeightTxtInput.Text.Substring(WeightTxtInput.Text.Length - 2) == "kg")
+              {
+                  WeightTxtInput.Text = WeightTxtInput.Text;
+              }
+             }
+             else
+             {
+             WeightTxtInput.Text = WeightTxtInput.Text + " kg";
+             }
+             var indexToSet = WeightTxtInput.Text.Length - 3;
+             var positionToSet = WeightTxtInput.GetPosition(WeightTxtInput.BeginningOfDocument, indexToSet);
+             WeightTxtInput.SelectedTextRange = WeightTxtInput.GetTextRange(positionToSet, positionToSet);
+         
+         });
+    }
+    
+    //Give user weight to CalculateVC
+    public override void prepareForSegue(UIStoryboardSegue segue, NSObject sender)
+    {
+        base.PrepareForSegue(segue, sender);
+
+        var Controller = segue.DestinationViewController as CalculateViewController;
+
+        if (Controller != null)
+        {
+            Controller.userWeightInKg = WeightTxtInput.Text.Remove(WeightTxtInput.Text.Length - 3, 3);
+
+           
+        }
     }
 
 
